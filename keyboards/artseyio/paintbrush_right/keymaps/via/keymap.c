0cd04671d5e0d44812d6cdb73f8ee06218d1d5f9
Riley <12001125+SonarMonkey@users.noreply.github.com>
2025-09-09 22:51:43 -0600

    Add basic OLED layer display

--- keyboards/artseyio/paintbrush_right/keymaps/via/keymap.c
+++ keyboards/artseyio/paintbrush_right/keymaps/via/keymap.c
@@ -25,8 +25,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
 
+    switch (get_highest_layer(layer_state)) {
+        case _ART_BASE:
+            oled_write_P(PSTR("BASE\n"), false);
+            break;
+        case _ART_NUM:
+            oled_write_P(PSTR("NUM\n"), false);
+            break;
+        case _ART_PUNC:
+            oled_write_P(PSTR("PUNC\n"), false);
+            break;
+      case _ART_CUS:
+            oled_write_P(PSTR("CUS\n"), false);
+            break;
+      case _ART_SYM:
+            oled_write_P(PSTR("SYM\n"), false);
+            break;
+      case _ART_MOU:
+            oled_write_P(PSTR("MOU\n"), false);
+            break;
+      case _ART_NAV:
+            oled_write_P(PSTR("NAV\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
 
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    
+    return false;
+}
+#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
